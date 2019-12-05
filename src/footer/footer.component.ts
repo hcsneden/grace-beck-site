8961e9a47a8ffdba0407be8145d257fd6ddc9351
hcsneden <48335502+hcsneden@users.noreply.github.com>
2019-12-04 22:14:15 -0600

    adding footer

---
+++ src/footer/footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'footer-component',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+
+export class FooterComponent {
+
+}
