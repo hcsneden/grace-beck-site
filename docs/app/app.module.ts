73b5ce20c1f46acda2b222aad8bd7a04d96882ea
hcsneden <48335502+hcsneden@users.noreply.github.com>
2020-04-30 15:40:53 -0500

    set useHash to true

--- docs/app/app.module.ts
+++ docs/app/app.module.ts
@@ -20,7 +20,7 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes),
+    RouterModule.forRoot(appRoutes, { useHash: true }),
     HttpClientModule
   ],
   providers: [],
