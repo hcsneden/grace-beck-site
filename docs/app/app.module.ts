1921d3f04ae2fb3a99dd6f2aad8ea86d49ffb01e
hcsneden <48335502+hcsneden@users.noreply.github.com>
2020-04-30 12:04:06 -0500

    chaning structure

--- docs/app/app.module.ts
+++ docs/app/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from 'docs/header/header.component';
-import { FooterComponent } from 'docs/footer/footer.component';
-import { BodyComponent } from 'docs/body/body.component';
+import { HeaderComponent } from 'docs/app/header/header.component';
+import { FooterComponent } from 'docs/app/footer/footer.component';
+import { BodyComponent } from 'docs/app/body/body.component';
 
 @NgModule({
   declarations: [
